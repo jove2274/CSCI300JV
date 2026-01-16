9c2e9d34d4e4243eae4a4c781141a55f73d2e6b8
jove2274 <Josie.Vallejo@colorado.edu>
2026-01-16 13:39:02 -0700

    Create HelloBlanks.cpp

---
+++ HelloBlanks.cpp
@@ -0,0 +1,14 @@
+// input output stream
+// C++ library
+#include <_______>                              // TODO: Include the correct library
+
+// standard namespace
+using namespace _______;                        // TODO: Specify the correct namespace
+
+_______ main() {                                // TODO: What variable type is the return type?
+    // Console output
+    _______ << "Hello World!\n";                // TODO: What is the output stream object?
+    _______ << "Hello World!" << _______;       // TODO What is the alt way to say next line?
+
+    return _______;                             // TODO What is the optional return value for a successful main function?
+}
